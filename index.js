fbb8084bdc70a31030ddd64d5e1306c0f2578a5c
Jyoti <jyotivermav2002@gmail.com>
2023-12-18 13:04:27 +0530

    stopwatch

---
+++ index.js
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StopWatch</title>
+<style>
+    body{
+        background-color: rgb(94, 48, 141);
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        height: 100vh;
+       
+    }
+    .div{
+        margin: 100px 30px 30px 30px;
+    }
+    #btn-container{
+        text-align: center;
+        margin-top: 50px;
+    }
+    #container{
+        color: rgb(228, 167, 174);
+    }
+    .digites{
+        font-size: 150px;
+        font-weight: bold;
+    }
+    .text{
+        font-size: 40px;
+        font-weight: bold;
+    }
+    .btn{
+        padding: 7px 25px;
+        font-weight: 18px;
+        margin: 0px 10px;
+        cursor: pointer;
+        border-radius: 20px;
+        border: 2px solid #15f4ee;
+    }
+    #start{
+        background-color: rgb(242, 67, 190);
+    }
+    #stop{
+        background-color: rgb(138, 243, 77);
+    }
+    #reset{
+        background-color: red;
+   }
+   .logo{
+    margin-top: 40px;
+   }
+   .bi {
+    color: rgba(245, 242, 106, 0.995);
+    margin: 5px 25px 5px 5px;
+   }
+</style>
+</head>
+<body>
+    <div class="div">
+    <div class="logo">
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-stopwatch-fill" viewBox="0 0 16 16">
+            <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584.531.531 0 0 0 .013-.012l.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354a.717.717 0 0 0-.012.012A6.973 6.973 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1h-3zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0z"/>
+          </svg>
+    </div>   
+    
+    <div id="container">
+        <div id="time">
+            <span class="digites" id="hr">00</span>
+            <span class="text">Hr</span>
+            <span class="digites" id="min">00</span>
+            <span class="text">Min</span>
+            <span class="digites" id="sec">00</span>
+            <span class="text">Sec</span>
+            <span class="digites" id="count">00</span>
+        </div>
+        <div id="btn-container">
+            <button class="btn" id="start" onclick="start()">Start</button>
+            <button class="btn" id="stop" onclick="stop()">Stop</button>
+            <button class="btn" id="reset" onclick="reset()">Reset</button>
+
+        </div>
+    </div>
+</div>
+</body>
+
+
+<script>
+            var hr =0 ;
+            var min =0 ;
+            var sec =0 ;
+            var count =0 ;
+
+    var timer =false;
+    function start(){
+        timer =true;
+        stopwatch();
+
+    }
+
+    function stop(){
+        timer =false;
+    }
+    function reset(){
+        timer =false;
+
+        hr =0;
+        min =0;
+        sec =0;
+
+        document.getElementById("hr").innerHTML = 00;
+        document.getElementById("min").innerHTML = 00;
+        document.getElementById("sec").innerHTML = 00;
+        document.getElementById("count").innerHTML = 00;
+    }
+
+    function stopwatch(){
+        if (timer == true){
+            count = count+1 ;
+
+            if(count == 100){
+                sec = sec+1 ;
+                count =0;
+            }
+            if(sec == 60){
+                min = min+1 ;
+                sec =0;
+                hr =0;
+                
+            }
+            if(min == 60){
+                hr = hr+1 ;
+                min =0;
+            }
+
+            
+        document.getElementById("hr").innerHTML = hr;
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("sec").innerHTML = sec;
+        document.getElementById("count").innerHTML = count;
+            setTimeout("stopwatch()",10);
+        }
+    }
+
+
+
+
+</script>
+</html>
